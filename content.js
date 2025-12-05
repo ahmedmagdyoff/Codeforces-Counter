48b4aa2d019930432c1d1937fdb214caf07c1b97
AHMED MAGDY <ahmedmagdy30003@gmail.com>
2025-12-05 16:47:50 +0200

    a

--- content.js
+++ content.js
@@ -1,56 +1,67 @@
 (() => {
     let allScores = {};
-    let standingsScores = {};
-    let unstandingsScores = {};
-    // async function fetchContentPage(groupId) { return new DOMParser().parseFromString(await (await fetch(`https://codeforces.com/group/${groupId}/contests`)).text(), "text/html"); }
+    async function fetchContentPage(groupId) { return new DOMParser().parseFromString(await (await fetch(`https://codeforces.com/group/${groupId}/contests`)).text(), "text/html"); }
     async function fetchStanding(url) {
+        let standings = {};
+        let unstandings = {};
         new DOMParser().parseFromString(await (await fetch(url)).text(), "text/html").querySelectorAll(".standings tr").forEach(row => {
             if (!row.querySelector(".rated-user")) return;
             const tds = row.querySelectorAll("td");
             const handle = tds[1].querySelector("a").innerText.trim();
-            const solved = parseInt(tds[2].innerText.trim());
-            if (tds[1].innerText.includes("*")) {
-                if (!unstandingsScores[handle]) unstandingsScores[handle] = 0;
-                unstandingsScores[handle] += solved;
-            } else {
-                if (!standingsScores[handle]) standingsScores[handle] = 0;
-                standingsScores[handle] += solved;
+            let solvedArray = [];
+            for (let i = 4; i < tds.length; i++) {
+                if (tds[i].querySelector(".cell-accepted")) solvedArray.push(1);
+                else solvedArray.push(0);
             }
+            if (tds[1].innerText.includes("*")) unstandings[handle] = solvedArray;
+            else standings[handle] = solvedArray;
         });
+        new Set([...Object.keys(standings), ...Object.keys(unstandings)]).forEach(handle => {
+            let allCounter = 0;
+            let standingsCounter = 0;
+            let unstandingsCounter = 0;
+            let standArr = standings[handle] || [];
+            let unstandArr = unstandings[handle] || [];
+            for (let i = 0; i < standArr.length; i++) {
+                if (standArr[i] === 1 || unstandArr[i] === 1) allCounter++;
+                standingsCounter += standArr[i] || 0;
+                unstandingsCounter += unstandArr[i] || 0;
+            }
+            if (!allScores[handle]) allScores[handle] = { allCounter: 0, standingsCounter: 0, unstandingsCounter: 0 };
+            allScores[handle].allCounter += allCounter;
+            allScores[handle].standingsCounter += standingsCounter;
+            allScores[handle].unstandingsCounter += unstandingsCounter;
+        });
+        // console.table(allScores);
     }
-    // function downloadCSV(result, name) {
-    //     let csv = "handle,solved\n";
-    //     result.forEach(row => { csv += `${row.handle},${row.solved}\n`; });
-    //     const blob = new Blob([csv], { type: "text/csv" });
-    //     const url = URL.createObjectURL(blob);
-    //     const a = document.createElement("a");
-    //     a.href = url;
-    //     a.download = `${name}.csv`;
-    //     a.click();
-    //     URL.revokeObjectURL(url);
-    // }
+    function downloadCSV(result, name) {
+        let csv = "handle,solved\n";
+        result.forEach(row => { csv += `${row.handle},${row.solved}\n`; });
+        const blob = new Blob([csv], { type: "text/csv" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = `${name}.csv`;
+        a.click();
+        URL.revokeObjectURL(url);
+    }
     async function run() {
-        // const match = window.location.href.match(/group\/([^\/]+)/);
-        // const groupId = match ? match[1] : null;
-        // if (!groupId) return;
-        // const content = await fetchContentPage(groupId);
-        let contentLinks = ['https://codeforces.com/group/ad1tgTiYqR/contest/648466'];
-        // content.querySelectorAll("table tr").forEach(row => {
-        //     const cols = row.querySelectorAll("td");
-        //     if (cols.length === 0) return;
-        //     const link = cols[0].querySelector("a");
-        //     if (!link) return;
-        //     contentLinks.push(link.href);
-        // });
+        const match = window.location.href.match(/group\/([^\/]+)/);
+        const groupId = match ? match[1] : null;
+        if (!groupId) return;
+        const content = await fetchContentPage(groupId);
+        // let contentLinks = ['https://codeforces.com/group/ad1tgTiYqR/contest/648466'];
+        let contentLinks = [];
+        content.querySelectorAll("table tr").forEach(row => {
+            const cols = row.querySelectorAll("td");
+            if (cols.length === 0) return;
+            const link = cols[0].querySelector("a");
+            if (!link) return;
+            contentLinks.push(link.href);
+        });
         await Promise.all(contentLinks.map(link => fetchStanding(link + "/standings")));
         let allResult = Object.entries(allScores).map(([handle, solved]) => ({ handle, solved })).sort((a, b) => b.solved - a.solved);
-        let standingsResult = Object.entries(standingsScores).map(([handle, solved]) => ({ handle, solved })).sort((a, b) => b.solved - a.solved);
-        let unstandingsResult = Object.entries(unstandingsScores).map(([handle, solved]) => ({ handle, solved })).sort((a, b) => b.solved - a.solved);
-        console.table(allResult);
-        console.table(standingsResult);
-        console.table(unstandingsResult);
-        // downloadCSV(standingsResult, "standingsResult");
-        // downloadCSV(unstandingsResult, "unstandingsResult");
+        downloadCSV(allResult, "allResult");
     }
     run();
 })();
